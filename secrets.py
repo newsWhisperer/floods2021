edab061d6344aad9be59ee1b3d28c8ba26afa5a7
KMicha <michael.kahle@yahoo.de>
2021-10-07 13:07:31 +0200

    add secrets

---
+++ secrets.py
@@ -0,0 +1,5 @@
+import os
+
+## setings for inquiring articles from newsapi.org
+#  Get API Key: https://newsapi.org/register &  https://newsapi.org/account
+os.environ['NEWSAPI_KEY'] = '1a2b3c4d5e6f7a8b9c0d1e2f3a4b5c6d7'
