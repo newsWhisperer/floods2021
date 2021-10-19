128814a883903b6a367effd6a1d95112a9e4e5d0
KMicha <michael.kahle@yahoo.de>
2021-10-19 10:05:05 +0200

    add API Key for geonames

--- secrets.orig.py
+++ secrets.orig.py
@@ -3,6 +3,11 @@ import os
 #> cp secrets.orig.py secrets.py
 #(git rm --cached secrets.py)
 
-## setings for inquiring articles from newsapi.org
+## settings for inquiring articles from newsapi.org
 #  Get API Key: https://newsapi.org/register &  https://newsapi.org/account
 os.environ['NEWSAPI_KEY'] = '1a2b3c4d5e6f7a8b9c0d1e2f3a4b5c6d7'
+
+## setting for inquring data from geonames
+# Get API Key here: http://www.geonames.org/export/web-services.html
+os.environ['GEONAMES_KEY'] = '1a2b3c4d5'
+
